18399fd6d70541bbdb3e96bd8319a5b539eb9648
nunes6786 <arthurnunes6786@gmail.com>
2025-09-24 11:34:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ function imc(){
         calcularIMC(){
             if (this.altura > 3) this.altura=this.altura/100
             const imc = this.peso/this.altura**2;
-            if (imc < 18.5 ) this.classificacao = "obesidade mÃ³rbida";
+            if (imc < 18.5 ) this.classificacao = "magreza";
             if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudÃ¡vel"
-            if (imc >= 25 &&  imc < 30 ) this.classificacao = "magreza"
+            if (imc >= 25 &&  imc < 30 ) this.classificacao = "sobbrepeso"
             if (imc >= 30 &&  imc < 35 ) this.classificacao = "obesidade"
             if (imc >= 35 &&  imc < 40 ) this.classificacao = "obesidade Severa"
             if (imc >= 40 ) this.classificacao = "obesidade mórbida"
